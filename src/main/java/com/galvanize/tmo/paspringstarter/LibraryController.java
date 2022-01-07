69775fa883ca524b0882e43ca8dff4d3ac0253d6
Carlos M Jaramillo <carlos.jaramillo@t-mobile.com>
2022-01-07 09:06:44 -0600

    Example Books

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.galvanize.tmo.paspringstarter.models.Book;
@@ -28,6 +29,7 @@ public class LibraryController {
     }
     
     @PostMapping("/api/books")
+    @ResponseStatus(HttpStatus.CREATED)
     public Book addBooks(@RequestBody Book book) {
     	return service.addBook(book);
     }
