632ec2f9b71b3e8eb93537e087740aad21f726b9
Carlos M Jaramillo <carlos.jaramillo@t-mobile.com>
2022-01-07 08:25:30 -0600

    Example Books

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,13 +1,46 @@
 package com.galvanize.tmo.paspringstarter;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.galvanize.tmo.paspringstarter.models.Book;
+import com.galvanize.tmo.paspringstarter.service.BookService;
 
 @RestController
 public class LibraryController {
 
+	private BookService service;
+
+	public LibraryController(BookService service) {
+		this.service = service;
+	}
+	
     @GetMapping("/health")
     public void health() {
 
     }
+    
+    @PostMapping("/api/books")
+    public Book addBooks(@RequestBody Book book) {
+    	return service.addBook(book);
+    }
+
+    @GetMapping("/api/books") 
+    public List<Book> getBooks() {
+    	return service.getBooks();
+    }
+    
+    @DeleteMapping("/api/books")
+    public ResponseEntity<Long> deleteBooks() {
+    	service.deleteBooks();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+       
 }
